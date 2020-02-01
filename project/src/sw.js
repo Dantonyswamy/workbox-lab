f23ef2c79ea7296e94235bfd21fbd0460016c166
Dantony <dantonyswamy@realtermenergy.com>
2020-02-01 11:37:24 -0500

    Workbox sample

--- project/src/sw.js
+++ project/src/sw.js
@@ -13,3 +13,64 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  /(.*)articles(.*)\.(?:png|gif|jpg)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  })
+);
+const articleHandler = workbox.strategies.networkFirst({
+  cacheName: 'articles-cache',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 50,
+    })
+  ]
+});
+
+workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+  return articleHandler.handle(args).then(response => {
+    if (!response) {
+      return caches.match('pages/offline.html');
+    } else if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  });
+});
+
+const postsHandler = workbox.strategies.networkFirst({
+  cacheName: 'posts-cache',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 50,
+    })
+  ]
+});
+workbox.routing.registerRoute(/(.*)post(.*)\.html/, args => {
+  return postsHandler.handle(args).then(response => {
+    if (!response) {
+      return caches.match('pages/offline.html');
+    } else if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  }).catch((e) => console.log(e))
+});
